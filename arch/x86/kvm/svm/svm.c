1e6a70f6f10eb5c1a9afa1da96fc617c9b1e75e8
Vaishali Thakkar <me.vaishalithakkar@gmail.com>
2025-06-20 11:28:01 +0200

    reorder MSRs
    Signed-off-by: Vaishali Thakkar <me.vaishalithakkar@gmail.com>

--- arch/x86/kvm/svm/svm.c
+++ arch/x86/kvm/svm/svm.c
@@ -142,7 +142,7 @@ static const struct svm_direct_access_msrs {
 	{ .index = X2APIC_MSR(APIC_TMICT),		.always = false },
 	{ .index = X2APIC_MSR(APIC_TMCCT),		.always = false },
 	{ .index = X2APIC_MSR(APIC_TDCR),		.always = false },
-
+	{ .index = MSR_AMD64_GUEST_TSC_FREQ,		.always = false },
 	/* Shadow Stack MSRs - Not intercepted for SNP guests */
 	{ .index = MSR_IA32_U_CET,			.always = false },
 	{ .index = MSR_IA32_S_CET,			.always = false },
@@ -151,7 +151,7 @@ static const struct svm_direct_access_msrs {
 	{ .index = MSR_IA32_PL2_SSP,			.always = false },
 	{ .index = MSR_IA32_PL3_SSP,			.always = false },
 	{ .index = MSR_IA32_INT_SSP_TAB,		.always = false },
-	{ .index = MSR_AMD64_GUEST_TSC_FREQ,		.always = false },
+	//{ .index = MSR_AMD64_GUEST_TSC_FREQ,		.always = false },
 	{ .index = MSR_INVALID,				.always = false },
 };
 
